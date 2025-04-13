80c6f1f182019769216843a2c881e56df5d15b9c
nikiaker <md.macek@gmail.com>
2025-04-13 23:26:01 +0200

    ulepszony kandydat

--- swaps.py
+++ swaps.py
@@ -60,13 +60,17 @@ def swap_nodes_between_cycles_with_candidates(first_cycle, second_cycle, distanc
     """
     n1, n2 = len(first_cycle) - 1, len(second_cycle) - 1
     for i in range(1, n1):
+        # Pobierz kandydatów dla wierzchołka z pierwszego cyklu
+        candidates = candidate_edges[first_cycle[i]]
         for j in range(1, n2):
-            # Sprawdź, czy wierzchołki są kandydatami
-            if second_cycle[j] in candidate_edges[first_cycle[i]] or first_cycle[i] in candidate_edges[second_cycle[j]]:
+            # Sprawdź, czy wierzchołek z drugiego cyklu jest kandydatem
+            if second_cycle[j] in candidates:
+                # Wprowadzenie krawędzi n1-n2
                 new_first = first_cycle[:]
                 new_second = second_cycle[:]
                 new_first[i], new_second[j] = new_second[j], new_first[i]
 
+                # Oblicz delta
                 delta = (distance_matrix[new_first[i - 1]][new_first[i]] +
                          distance_matrix[new_first[i]][new_first[i + 1]] +
                          distance_matrix[new_second[j - 1]][new_second[j]] +
@@ -84,12 +88,16 @@ def swap_edges_within_cycle_with_candidates(cycle, distance_matrix, candidate_ed
     """
     n = len(cycle) - 1
     for i in range(1, n - 1):
+        # Pobierz kandydatów dla wierzchołka cycle[i]
+        candidates = candidate_edges[cycle[i]]
         for j in range(i + 2, n):
-            # Sprawdź, czy wierzchołki są kandydatami
-            if cycle[j] in candidate_edges[cycle[i]]:
+            # Sprawdź, czy wierzchołek cycle[j] jest kandydatem
+            if cycle[j] in candidates:
+                # Wprowadzenie krawędzi n1-n2
                 new_cycle = cycle[:]
                 new_cycle[i:j + 1] = reversed(new_cycle[i:j + 1])
 
+                # Oblicz delta
                 delta = (distance_matrix[new_cycle[i - 1]][new_cycle[i]] +
                          distance_matrix[new_cycle[j]][new_cycle[j + 1]]) - (
                                 distance_matrix[cycle[i - 1]][cycle[i]] +
